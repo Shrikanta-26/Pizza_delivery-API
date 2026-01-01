7f609d58a01a18e06c80fe74ac6d22dc28250332
shrikanta <shrikantadutta108@gmail.com>
2026-01-02 01:23:33 +0600

    Setup custom user and database

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_("Email should be provided"))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_("Superuser must have is_staff=True"))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_("Superuser must have is_superuser=True"))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(max_length=80, unique=True)
+    phone_number = PhoneNumberField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone_number']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
