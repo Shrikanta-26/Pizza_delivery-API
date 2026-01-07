f334d14f41c742d1be6ff9a95f9285bc44bb818a
shrikanta <shrikantadutta108@gmail.com>
2026-01-07 23:59:51 +0600

    Auth customization

--- authentication/models.py
+++ authentication/models.py
@@ -6,36 +6,54 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, username, phone_number, password=None, **extra_fields):
         if not email:
-            raise ValueError(_("Email should be provided"))
+            raise ValueError(_("Email must be provided"))
+        if not username:
+            raise ValueError(_("Username must be provided"))
+        if not phone_number:
+            raise ValueError(_("Phone number must be provided"))
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        extra_fields.setdefault("is_active", True)
+
+        user = self.model(
+            email=email,
+            username=username,
+            phone_number=phone_number,
+            **extra_fields
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+    def create_superuser(self, email, username, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
-        if extra_fields.get('is_staff') is not True:
+        if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True"))
-        if extra_fields.get('is_superuser') is not True:
+        if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True"))
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(
+            email=email,
+            username=username,
+            phone_number=phone_number,
+            password=password,
+            **extra_fields
+        )
 
 
 class User(AbstractUser):
     username = models.CharField(max_length=25, unique=True)
     email = models.EmailField(max_length=80, unique=True)
-    phone_number = PhoneNumberField(null=False,unique=True)
+    phone_number = PhoneNumberField(unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'phone_number']
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "phone_number"]
 
     objects = CustomUserManager()
 
--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,40 +1,17 @@
-from .models import User
 from rest_framework import serializers
-from phonenumber_field.serializerfields import PhoneNumberField
+from .models import User
+
 
 class UserCreationSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=25,)
-    email = serializers.EmailField(max_length=80,)
-    phone_number = PhoneNumberField(allow_null=False,allow_blank=False)
-    password = serializers.CharField(min_length=8,write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ['username','email','phone_number','password']
+        fields = ("email", "username", "phone_number", "password")
+        extra_kwargs = {"email": {"error_messages": { "unique": "Email already exists"}},
+                        "username": { "error_messages": { "unique": "Username already exists"}},
+                        # "phone_number": {"error_messages": {"unique": "Phone number already exists"}},
+        }
 
-    def validate(self, attrs):
-        username_exists = User.objects.filter(username=attrs['username']).exists()
-
-        if username_exists:
-            raise serializers.ValidationError(detail="User with username exists")
-        
-        email_exists = User.objects.filter(username=attrs['email']).exists()
-
-        if email_exists:
-            raise serializers.ValidationError(detail="User with email exists")
-        phonenumber_exists = User.objects.filter(username=attrs['phone_number']).exists()
-
-        if phonenumber_exists:
-            raise serializers.ValidationError(detail="User with phonenumber exists")
-       
-        return super().validate(attrs) 
     def create(self, validated_data):
-        user=User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            phone_number=validated_data['phone_number']
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        return User.objects.create_user(**validated_data)
